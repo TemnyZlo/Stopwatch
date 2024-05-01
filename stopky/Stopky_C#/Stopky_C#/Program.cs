fe4b0382631a06a001107c646fa177a8f7ca0cf5
Filip Ebert <65273341+TemnyZlo@users.noreply.github.com>
2024-05-01 11:43:59 +0200

    Add files via upload

---
+++ stopky/Stopky_C#/Stopky_C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using Stopky_C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StopwatchManager manager = new StopwatchManager();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("1: Start 2: Pause 3: Stop 4: Reset 5: Save Time 6: Load Times 7: Exit");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    manager.Start();
+                    break;
+                case ConsoleKey.D2:
+                    manager.Pause();
+                    break;
+                case ConsoleKey.D3:
+                    manager.Stop();
+                    break;
+                case ConsoleKey.D4:
+                    manager.Reset();
+                    break;
+                case ConsoleKey.D5:
+                    Console.WriteLine("\nEnter name to save time:");
+                    string name = Console.ReadLine();
+                    manager.SaveTime(name);
+                    break;
+                case ConsoleKey.D6:
+                    var times = manager.LoadTimes();
+                    foreach (var time in times)
+                        Console.WriteLine($"{time.Key}: {time.Value}");
+                    break;
+                case ConsoleKey.D7:
+                    running = false;
+                    break;
+            }
+            Console.WriteLine($"Elapsed Time: {manager.GetElapsedTime()}");
+        }
+    }
+}
---
+++ stopky/Stopky_C#/Stopky_C#/StopwatchManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Stopky_C_
+{
+    internal class StopwatchManager
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private Dictionary<string, TimeSpan> savedTimes = new Dictionary<string, TimeSpan>();
+
+        public void Start()
+        {
+            if (!stopwatch.IsRunning)
+                stopwatch.Start();
+        }
+
+        public void Pause()
+        {
+            if (stopwatch.IsRunning)
+                stopwatch.Stop();
+        }
+
+        public void Stop()
+        {
+            if (stopwatch.IsRunning)
+                stopwatch.Stop();
+        }
+
+        public void Reset()
+        {
+            stopwatch.Reset();
+        }
+
+        public TimeSpan GetElapsedTime()
+        {
+            return stopwatch.Elapsed;
+        }
+
+        public void SaveTime(string name)
+        {
+            savedTimes[name] = stopwatch.Elapsed;
+            File.WriteAllText("times.json", JsonSerializer.Serialize(savedTimes));
+        }
+
+        public Dictionary<string, TimeSpan> LoadTimes()
+        {
+            if (File.Exists("times.json"))
+            {
+                string json = File.ReadAllText("times.json");
+                savedTimes = JsonSerializer.Deserialize<Dictionary<string, TimeSpan>>(json);
+            }
+            return savedTimes;
+        }
+    }
+}
+
